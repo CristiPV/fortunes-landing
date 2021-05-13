c1a1881b7184bcef738c63bd09c72183f38cda70
Cristian <cristi.valentin29@gmail.com>
2021-05-13 19:38:38 +0200

    Add historyPage
    Added a page that displays a table of previous winnings and a button to go back.

--- frontend/fortunes-landing/src/pages/historyPage/historyPage.js
+++ frontend/fortunes-landing/src/pages/historyPage/historyPage.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import { Container, Table, Button } from 'react-bootstrap';
+
+import './historyPage.css';
 
 function HistoryPage( props ) {
+    const location = useLocation();
+
+    const prizeHistory = location.state.prizeHistory;
+
     return (
-        <div className="history-page">
-            History
-        </div>
+        <Container className="history-page">
+            <Container className="link-container">
+                <Link 
+                    to={ {
+                        pathname: "/wheel"
+                    } }
+                >
+                    <Button variant="warning">Back</Button>
+                </Link>
+            </Container>
+
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Prize</th>
+                        <th>Weight</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        prizeHistory.map( ( prize, index ) => {
+                            return(
+                                <tr key={ index }>
+                                    <td>
+                                        { index }
+                                    </td>
+                                    <td>
+                                        { prize.name }
+                                    </td>
+                                    <td>
+                                        { prize.weight }
+                                    </td>
+                                </tr>
+                            );
+                        } )
+                    }
+                </tbody>
+            </Table>
+        </Container>
     );
 }
 
